b26ab877c67884a78b6520e1535b6ab650373a91
ManiacD16 <msd161918@gmail.com>
2024-07-20 17:21:11 +0530

    hii

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { News, User } = require("./models/News"); // Your News model
 
 // MongoDB connection
